1a3bf4e346c118f0d2f9a6ea7f5e639eb27ad095
Galaxy <62033325+GalaxyComic@users.noreply.github.com>
2024-11-20 11:48:32 +0100

    added enitycore framework

---
+++ PRA-C3-DJ-SA-CH-AL/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRA_C3_DJ_SA_CH_AL.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string PasswordHashed { get; set; }
+        public int Credits { get; set; }
+    }
+}
---
+++ PRA-C3-DJ-SA-CH-AL/UserDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System;
+
+namespace PRA_C3_DJ_SA_CH_AL
+{
+    public class UserDbContext : DbContext
+    {
+        public DbSet<Models.User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                "server=localhost;" +                                               // Server name
+                "port=3306;" +                                                      // Server port
+                "user=root;" +                                                      // Username
+                "password=;" +                                                      // Password
+                "database=django_test;"                                             // Database name
+                , Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.30-mysql") // Version
+                );
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
